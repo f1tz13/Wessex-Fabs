b65319be59a45428697fb42779db098213693b14
f1tz13 <63474179+f1tz13@users.noreply.github.com>
2025-11-06 12:34:03 +0000

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily:'sans-serif',padding:'40px'}}>
-      <h1>Wessex Fabrications Ltd</h1>
-      <p>✅ Deployment successful</p>
-      <p>This is the starter. I’ll swap in the full site after it’s live.</p>
-    </div>
-  )
-}
